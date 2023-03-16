116f180f7aa33d058cc250d975c3514459037134
Ntlakala <tlaki.sela@gmail.com>
2023-03-16 02:46:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
-
+#include <unistd.h>
+#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
-return (0);
+write(2, MESSAGE, sizeof(MESSAGE) - 1);
+return (1);
 }
