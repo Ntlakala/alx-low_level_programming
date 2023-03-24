a364cfd0efa3d2c656c06bd82a49da099e72c6e6
Ntlakala <tlaki.sela@gmail.com>
2023-03-24 13:10:53 -0700

    0x03-debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+   /**
+   * main - Entry point
+   *
+   * Return: Always 0 (Success)
+   */
+  int main(void)
+  {
+ int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d is ", n);
+if (n > 0)
+ printf("positive");
+else if (n == 0)
+printf("zero");
+else
+printf("negative");
+printf("\n");
+ return (0);
+}
+
