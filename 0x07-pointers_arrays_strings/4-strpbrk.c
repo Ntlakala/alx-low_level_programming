7a377a664ec4fe8fc5adf1455eeb85486b38d343
Ntlakala <tlaki.sela@gmail.com>
2023-04-04 12:04:46 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
  * @s: pointer to string to search
  * @accept: pointer to string of bytes to search for
  *
- * Return: a pointer to the byte in s that matches one of the bytes in accep
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
  *         or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
