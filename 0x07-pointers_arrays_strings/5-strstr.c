11969c6ed8efc5b7aa49b06c2d89ef4ce0aa68c5
Ntlakala <tlaki.sela@gmail.com>
2023-04-04 12:43:35 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h
 
 /**
-* _strstr - finds the first occurrence of the substring needle in the
-* string haystack.
-* @haystack: pointer to string to search in
-* @needle: pointer to substring to search for
-*
-* Return: pointer to the beginning of the located substring, or NULL if
-* the substring is not found
-*/
+ * _strstr - finds the first occurrence
+ * of the substring needle in the string haystack
+ * @haystack: pointer to string to search in
+ * @needle: pointer to substring to search for
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	size_t i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
@@ -24,5 +23,7 @@ char *_strstr(char *haystack, char *needle)
 		if (!needle[j])
 			return (&haystack[i]);
 	}
+
 	return (NULL);
 }
+
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
  * print_chessboard - prints the chessboard
- * @a: two dimension array to print
+ * @a: the chessboard to print
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+    int row, col;
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
-	}
+    for (row = 0; row < 8; row++)
+    {
+        for (col = 0; col < 8; col++)
+        {
+            _putchar(a[row][col]);
+        }
+        _putchar('\n');
+    }
 }
+
