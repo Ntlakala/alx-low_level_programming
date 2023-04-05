819fcc4c2d66e27e20d0fc7b0a2b15bf4a3c226e
Ntlakala <tlaki.sela@gmail.com>
2023-04-05 13:18:40 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,23 +1,33 @@
+#include "main.h"
+
 /**
- * is_palindrome - Checks if a string is a palindrome
- * @s: The string to check
+ * _strlen_recursion - returns the length of a string
+ * @s: input string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: input string
  *
  * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-    int len = _strlen_recursion(s);
+	int len = _strlen_recursion(s);
 
-    /* empty string or single character is a palindrome */
-    if (len <= 1)
-        return (1);
-
-    /* compare first and last characters */
-    if (*s != s[len - 1])
-        return (0);
-
-    /* recursively check the remaining substring */
-    s[len - 1] = '\0'; /* temporarily remove the last character */
-    return (is_palindrome(s + 1));
+	if (len <= 1)
+		return (1);
+	if (*s == *(s + len - 1))
+		return (is_palindrome(s + 1) && 1);
+	else
+		return (0);
 }
 
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,40 +1,55 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if they can be considered
- * identical, otherwise returns 0
- * @s1: first string
- * @s2: second string, can contain the special character *
+ * wildcmp_helper - compares two strings recursively, allowing for wildcard
+ * @s1: first string to compare
+ * @s2: second string to compare
  *
- * Return: 1 if the strings are identical, otherwise 0
+ * Return: 1 if strings match, 0 otherwise
+ */
+int wildcmp_helper(char *s1, char *s2)
+{
+    if (*s2 == '*')
+    {
+        if (*(s2 + 1) == '*')
+            return (wildcmp_helper(s1, s2 + 1));
+        else if (*s1 == '\0')
+            return (wildcmp_helper(s1, s2 + 1));
+        else
+            return (wildcmp_helper(s1 + 1, s2) || wildcmp_helper(s1, s2 + 1));
+    }
+    else if (*s1 != *s2)
+        return (0);
+    else if (*s1 == '\0' && *s2 == '\0')
+        return (1);
+    else
+        return (wildcmp_helper(s1 + 1, s2 + 1));
+}
+
+/**
+ * wildcmp - compares two strings recursively, allowing for wildcard
+ * @s1: first string to compare
+ * @s2: second string to compare (may contain wildcard)
+ *
+ * Return: 1 if strings match, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-    /* if both strings are empty, they're identical */
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
-
-    /* if the current characters match, move to the next */
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
-
-    /* if s2 has a wildcard, try to match the rest of s1 */
     if (*s2 == '*')
     {
-        /* skip consecutive wildcards */
-        while (*(s2 + 1) == '*')
-            s2++;
-
-        /* if the wildcard is the last character, they match */
         if (*(s2 + 1) == '\0')
             return (1);
-
-        /* try to match the rest of s1 after the wildcard */
-        if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-            return (1);
+        else
+            return (wildcmp_helper(s1, s2));
     }
-
-    /* if we got here, the strings are different */
-    return (0);
+    else
+    {
+        if (*s1 != *s2)
+            return (0);
+        else if (*s1 == '\0' && *s2 == '\0')
+            return (1);
+        else
+            return (wildcmp(s1 + 1, s2 + 1));
+    }
 }
 
