0f52f3c4a539a1c886c3bd5f82e1efee55d08c26
Ntlakala <tlaki.sela@gmail.com>
2023-04-13 07:15:18 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,15 +4,14 @@
 /**
  * *_realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
+ * @old_size: size of the allocated memory for pntr
  * @new_size: new size of the new memory block
- *
- * Return: pointer to the newly allocated memory block
+ * Return: pointr to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *pntr1;
-	char *old_pnt;
+	char *ptr1;
+	char *old_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -27,24 +26,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr)
 		return (malloc(new_size));
 
-	pntr1 = malloc(new_size);
+	ptr1 = malloc(new_size);
 	if (!ptr1)
 		return (NULL);
 
-	old_pnt = ptr;
+	old_ptr = ptr;
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			pntr1[i] = old_pnt[i];
+			ptr1[i] = old_ptr[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			pntr1[i] = old_pnt[i];
+			ptr1[i] = old_ptr[i];
 	}
 
 	free(ptr);
-	return (pntr1);
+	return (ptr1);
 }
