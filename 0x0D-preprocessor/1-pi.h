1fc6e7ee1a56b528c2353f019138793349f3af84
Ntlakala <tlaki.sela@gmail.com>
2023-04-17 06:02:38 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef PI_h_
+#define PI_h
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    printf(__FILE__ "\n");
+    return 0;
+}
+
