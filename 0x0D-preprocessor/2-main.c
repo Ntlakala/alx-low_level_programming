a82753282814c85377ebbee171c903ff36760779
Ntlakala <tlaki.sela@gmail.com>
2023-04-17 06:19:23 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
+ * main - Entry point
+ *
+ * Description: This function prints the name of the current source file
+ * to the standard output, followed by a newline character.
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf(__FILE__, "\n");
-	return (0);
+        printf(__FILE__ "\n");
+        return (0);
 }
 
