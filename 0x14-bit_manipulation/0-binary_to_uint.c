5c88421a482e5291dbaacb0b50e9159abdbec313
Ntlakala <tlaki.sela@gmail.com>
2023-05-06 20:08:46 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,6 +4,7 @@
 /**
  * binary_to_uint convrt a binary number to an unsigned int
  * @b: char string
+ *
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
